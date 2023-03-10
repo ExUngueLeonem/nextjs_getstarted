8a5cb968c016e600ddacd8de899f366ad30b835b
Ilya Krukov <ujfox13@gmail.com>
2023-03-10 16:26:30 +0300

    layout

---
+++ components/common/Layout/index.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from "react";
+
+import styles from './Layout.module.scss';
+
+const Layout = ({ children }: { children: ReactNode }) => {
+    return (
+        <div className={styles.container}>
+            {children}
+        </div>
+    );
+};
+
+export default Layout;
